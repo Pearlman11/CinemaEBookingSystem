7a1a84e67572ad87879ead85ebca1f4039685948
patrickkelly3 <p3kellyga@gmail.com>
2025-02-18 13:11:49 -0500

    Created backend file structure using spring

---
+++ backend/src/test/java/com/SWE/CinemaEBookingSystem/CinemaEBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SWE.CinemaEBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaEBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
