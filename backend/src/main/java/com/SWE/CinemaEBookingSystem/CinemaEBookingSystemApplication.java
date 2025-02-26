0ef6d4a726dbce267da0f4181202ea7e1de9df57
patrickkelly3 <p3kellyga@gmail.com>
2025-02-25 20:33:48 -0500

    Added more dummy data

--- backend/src/main/java/com/SWE/CinemaEBookingSystem/CinemaEBookingSystemApplication.java
+++ backend/src/main/java/com/SWE/CinemaEBookingSystem/CinemaEBookingSystemApplication.java
@@ -37,19 +37,71 @@ public class CinemaEBookingSystemApplication {
 			}
 	
 			List<Movie> movies = List.of(
-				new Movie(
-					"The Monkey",
-					"Now Playing",
-					List.of("Theo James", "Elijah Wood"),
-					"Osgood Perkins",
-					"James Wan",
-					"https://www.youtube.com/watch?v=1jc0KjSiXb0",
-					"https://www.impawards.com/2025/posters/monkey_ver2_xlg.jpg",
-					"Twin brothers Hal and Bill discover a cursed toy monkey...",
-					Set.of("Horror gold!"),
-					MovieRating.R,
-					new ArrayList<>()
-				)
+					new Movie(
+						"The Monkey",
+						"Now Playing",
+						List.of("Theo James", "Elijah Wood", "Tatiana Maslany", "Christian Convery"),
+						"Osgood Perkins",
+						"James Wan",
+						"https://www.youtube.com/watch?v=1jc0KjSiXb0",
+						"https://www.impawards.com/2025/posters/monkey_ver2_xlg.jpg",
+						"Twin brothers Hal and Bill discover a cursed toy monkey in their attic...",
+						Set.of(
+							"The Monkey is 90 minutes of pure bloody, comedic horror gold.",
+							"A gory, surprisingly existential horror comedy that embraces Perkins’ silly side."
+						),
+						MovieRating.R,
+						new ArrayList<>()
+					),
+					new Movie(
+						"Flight Risk",
+						"Now Playing",
+						List.of("Mark Wahlberg", "Michelle Dockery", "Topher Grace", "Jonathan Sadowski"),
+						"Mel Gibson",
+						"John Davis",
+						"https://www.youtube.com/watch?v=Cml3CFDBj2s",
+						"https://assets.voxcinemas.com/posters/P_HO00011875_1736503368670.jpg",
+						"A pilot transporting a fugitive across Alaska finds himself entangled in a deadly game...",
+						Set.of(
+							"Flight Risk was surprisingly better than expected.",
+							"The immersive effects add an extra layer of excitement."
+						),
+						MovieRating.R,
+						new ArrayList<>()
+					),
+					new Movie(
+						"Longlegs",
+						"Now Playing",
+						List.of("Maika Monroe", "Nicolas Cage", "Blair Underwood", "Alicia Witt"),
+						"Osgood Perkins",
+						"Jennifer Sharp",
+						"https://www.youtube.com/watch?v=OG7wOTE8NhE",
+						"https://yc.cldmlk.com/q201nsmb396zm2anq05v95jfh4/1719341480737_Poster.jpg",
+						"FBI agent Lee Harker investigates a series of occult-driven murders...",
+						Set.of(
+							"A chilling and atmospheric horror film that delivers on its premise.",
+							"Nicolas Cage gives a standout performance."
+						),
+						MovieRating.R,
+						new ArrayList<>()
+					),
+					new Movie(
+						"Captain America: Brave New World",
+						"Now Playing",
+						List.of("Anthony Mackie", "Harrison Ford", "Liv Tyler", "Giancarlo Esposito"),
+						"Julius Onah",
+						"Kevin Feige",
+						"https://www.youtube.com/watch?v=O_AcR5YkU5Y",
+						"https://m.media-amazon.com/images/M/MV5BNDRjY2E0ZmEtN2QwNi00NTEwLWI3MWItODNkMGYwYWFjNGE0XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+						"Sam Wilson, now Captain America, faces a global conspiracy...",
+						Set.of(
+							"Anthony Mackie shines as the new Cap in this thrilling MCU entry.",
+							"A solid mix of action and political intrigue."
+						),
+						MovieRating.PG13,
+						new ArrayList<>()
+					)
+			
 			);
 	
 			movieRepository.saveAll(movies);
