4fede47fd1c2d1726bfa49dc61cc31388e10b113
patrickkelly3 <p3kellyga@gmail.com>
2025-02-26 13:35:29 -0500

    Added repository code for showtime

--- backend/src/main/java/com/SWE/CinemaEBookingSystem/repository/ShowTimeRepository.java
+++ backend/src/main/java/com/SWE/CinemaEBookingSystem/repository/ShowTimeRepository.java
@@ -1,5 +1,9 @@
 package com.SWE.CinemaEBookingSystem.repository;
 
-public class ShowTimeRepository {
-    
+import com.SWE.CinemaEBookingSystem.entity.Showtime;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShowtimeRepository extends JpaRepository<Showtime, Long> {
 }
