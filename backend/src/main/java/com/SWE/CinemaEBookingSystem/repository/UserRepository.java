938c982883d44ac92384c2900e47c4ae9e810a14
patrickkelly3 <p3kellyga@gmail.com>
2025-03-17 17:46:32 -0400

    User service and repository

---
+++ backend/src/main/java/com/SWE/CinemaEBookingSystem/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.SWE.CinemaEBookingSystem.repository;
+
+import com.SWE.CinemaEBookingSystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email); // To check if email is already registered
+}
