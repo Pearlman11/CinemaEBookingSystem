0218324a20c8435b124d6cdd1e985ece034e68ad
Easwar Senthilkumar <easwarsenthilkumar@gmail.com>
2025-03-19 01:00:27 -0400

    Add PaymentCard entity

---
+++ backend/src/main/java/com/SWE/CinemaEBookingSystem/entity/PaymentCards.java
@@ -0,0 +1,51 @@
+package com.SWE.CinemaEBookingSystem.entity;
+import jakarta.persistence.*; 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+
+@Entity
+@Table(name = "PaymentCards")
+public class PaymentCards {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "card_id")
+    private Integer id;
+
+    @Column(name = "card_Number")
+    private String cardNumber;
+
+    @Column(name = "billing_Address")
+    private String billingAddress;
+
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "expiration_Date")
+    private Date expirationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+
+    public PaymentCards(){};
+    public PaymentCards(String cardNumber,String billingAddress,Date expirationDate){
+        this.cardNumber = cardNumber;
+        this.billingAddress = billingAddress;
+        this.expirationDate = expirationDate;
+    }
+
+    // Getters
+    public String getCardNumber() { return cardNumber; }
+    public String getBillingAddress() { return billingAddress; }
+    public Date getExpirationDate() { return expirationDate; }
+    public User getUser(){return user;}
+
+    //Setters
+    public void setCardNumber(String cardNumber) { this.cardNumber = cardNumber; }
+    public void setBillingAddress(String billingAddress) { this.billingAddress = billingAddress; }
+    public void setExpirationDate(Date expirationDate) { this.expirationDate = expirationDate; }
+    public void setUser(User user) {this.user = user;}
+
+}
